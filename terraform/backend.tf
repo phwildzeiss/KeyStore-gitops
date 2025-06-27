f6ca2cf905987c9bf26f29af25406ae0fefd0903
Philipp Wildzeiss <philipp.wildzeiss@aon.at>
2025-06-27 20:03:50 +0200

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     endpoint                    = "https://sos-at-vie-1.exo.io" # je nach Region z. B. sos-ch-dk-2.exo.io
-    bucket                      = "my-terraform-state"
+    bucket                      = "my-terraform-state-test"
     key                         = "infra/terraform.tfstate"
     region                      = "us-east-1"
     force_path_style            = true # nötig für Ceph‑S3‑Kompatibilität
